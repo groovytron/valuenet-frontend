9c8ac41bb9891a9716341d0c737ea13533093cb9
Ursin <ursin.brunner@gmail.com>
2020-08-31 08:12:17 +0200

    Automatize build.

--- build_docker.sh
+++ build_docker.sh
@@ -1,3 +1,9 @@
+npm install
+npm run build
+
 cp -R build docker
 cd docker
 docker build -t ursinbrunner/proton-inference-frontend:latest .
+docker push ursinbrunner/proton-inference-frontend:latest
+
+rm -R $(ls -I "Dockerfile" )
